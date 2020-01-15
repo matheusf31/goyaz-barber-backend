aa805db2bbf2daa071a5a9a725746b828ef8e563
Matheus Filipe <mfcr031@gmail.com>
2020-01-15 14:08:32 -0300

    backend

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import './config/ReactotronConfig';
+
 const App = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
