a197f1e4dafcacdb3f54bbab363e84b9af4ee2a1
Matheus Filipe <mfcr031@gmail.com>
2020-01-16 12:35:07 -0300

    migration de usuário

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,16 @@
-/* 
+/*
   credenciais para acessar a base de dados
 */
 
 module.exports = {
   dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'goyazbarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
 };
