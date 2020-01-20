8d1429732da8096a189a332e92d30a7281db63a7
Matheus Filipe <mfcr031@gmail.com>
2020-01-20 17:05:23 -0300

    update users

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -8,12 +8,36 @@ class UserController {
       },
     });
 
+    // interrompe o fluxo se já existir um usuário
     if (userExists) {
-      // interrompe o fluxo
       return res.status(400).json({ error: 'User already exists.' });
     }
 
     const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({ id, name, email, provider });
+  }
+
+  // usuário tem que estar logado
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    // verifica se o email que está sendo alterado é diferente do email antigo
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
 
     return res.json({ id, name, email, provider });
   }
---
+++ backend/src/app/middlewares/auth.js
@@ -0,0 +1,31 @@
+/*
+  Middleware de autenticação;
+  Verificar se usuário está logado;
+*/
+
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.send(401).json({ error: 'Token not provided' });
+  }
+
+  // retorna um array com duas posições e o segundo elemento é o que interessa
+  const [, token] = authHeader.split(' ');
+
+  // o procedimento pode retornar erro, por isso o try/catch
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid' });
+  }
+};
