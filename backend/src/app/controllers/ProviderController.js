7dc65f5f086f2211c3047da9cb480c7126639b47
Matheus Filipe <mfcr031@gmail.com>
2020-01-23 16:48:18 -0300

    listagem de prestadores de serviço

---
+++ backend/src/app/controllers/ProviderController.js
@@ -0,0 +1,27 @@
+/*
+  Novo controller pois quero listar apenas um tipo de usuário
+  No controller de usuário é listado todo tipo de usuário
+*/
+
+import User from '../models/User';
+import File from '../models/File';
+
+class ProviderController {
+  async index(req, res) {
+    const providers = await User.findAll({
+      where: { provider: true },
+      attributes: ['id', 'name', 'email', 'avatar_id'],
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['name', 'path', 'url'],
+        },
+      ],
+    });
+
+    return res.json(providers);
+  }
+}
+
+export default new ProviderController();
