9f2524def7d53acb11d4d6c9f47ff016e5d3e6ee
Matheus Filipe <mfcr031@gmail.com>
2020-01-24 16:35:59 -0300

    Validação de agendamento

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -1,4 +1,6 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { check } from 'prettier';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -26,11 +28,35 @@ class AppointmentController {
         .json({ error: 'You can only appointment with providers ' });
     }
 
+    /* 
+      parseISO vai transformar a string passada no req.body em um objeto DATE
+      startOfHower vai pegar apenas o início da hora
+      OBSERVAÇÃO: PROVAVELMENTE TEREI QUE ALTERAR ESSA LINHA PARA SE ENCAIXAR COM OS HORÁRIOS DO THIADO
+    */
+
+    // Checando se a data/hora escolhida está ANTES da data/hora atual
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permited' });
+    }
+
+    // Checando se o provedor já não tem um agendamento marcado pro mesmo horário
+    const checkAvailability = await Appointment.findOne({
+      where: { provider_id, canceled_at: null, date: hourStart },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
+    }
+
     // Como passou todas as verificações vou criar o agendamento na DB
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart,
     });
 
     return res.json(appointment);
