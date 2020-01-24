4c48ef37d2ae85a9159a267ef87a0eb4d6459e8c
Matheus Filipe <mfcr031@gmail.com>
2020-01-24 15:51:10 -0300

    migration e model de agendamento

--- backend/src/app/controllers/ProviderController.js
+++ backend/src/app/controllers/ProviderController.js
@@ -1,5 +1,5 @@
 /*
-  Novo controller pois quero listar apenas um tipo de usuário
+  Novo controller pois quero listar apenas um tipo de usuário (os providers)
   No controller de usuário é listado todo tipo de usuário
 */
 
---
+++ backend/src/app/models/Appointment.js
@@ -0,0 +1,25 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Appoitments extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        date: Sequelize.DATE,
+        canceled_at: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    // pertence ao model de usuário pq 1 usuário fez o agendamento
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
+  }
+}
+
+export default Appoitments;
