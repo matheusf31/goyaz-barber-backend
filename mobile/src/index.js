0525aee283ac14ddfef700e09584353c9b436b1d
Matheus Filipe <mfcr031@gmail.com>
2020-02-27 09:43:23 -0300

    start mobile

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
 
-import Teste from '~/Teste';
-
 // import { Container } from './styles';
 
 export default function App() {
-  return <Teste />;
+  return <View />;
 }
