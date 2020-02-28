831d6139466ff833138a994988ac4fc49432a18f
Matheus Filipe <mfcr031@gmail.com>
2020-02-28 14:22:43 -0300

    redux persist work

--- mobile/src/store/persistReducers.js
+++ mobile/src/store/persistReducers.js
@@ -6,7 +6,7 @@ export default reducers => {
     {
       key: 'root',
       storage: AsyncStorage,
-      whitelist: ['auth, user'],
+      whitelist: ['auth', 'user'],
     },
     reducers
   );
