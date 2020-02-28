dcf55d0b347bbd1ae31893f4ea553e0dbfb89bc3
Matheus Filipe <mfcr031@gmail.com>
2020-02-28 14:34:55 -0300

    exibindo alert

--- mobile/src/store/modules/auth/sagas.js
+++ mobile/src/store/modules/auth/sagas.js
@@ -1,22 +1,27 @@
+import { Alert } from 'react-native';
 import { all, takeLatest, call, put } from 'redux-saga/effects';
-
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 import api from '~/services/api';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'sessions', { email, password });
+    const response = yield call(api.post, 'sessions', { email, password });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (user.provider) {
-    console.tron.error('Usuário é um prestador de serviço');
-    return;
+    if (user.provider) {
+      Alert.alert('Usuário é um prestador de serviço');
+      return;
+    }
+
+    yield put(signInSuccess(token, user));
+  } catch (err) {
+    Alert.alert('Falha na autenticação, verifique seus dados');
+    yield put(signFailure());
   }
-
-  yield put(signInSuccess(token, user));
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
