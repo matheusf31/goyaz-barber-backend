134df1be2f160ad86b6c8ae39bf5f3b33bbac66e
Matheus Filipe <mfcr031@gmail.com>
2020-03-02 19:32:51 -0300

    cadastro e navegação finalizado

--- mobile/src/components/Background/index.js
+++ mobile/src/components/Background/index.js
@@ -2,10 +2,10 @@ import styled from 'styled-components/native';
 
 import background from '~/assets/images/background.jpg';
 
-export default styled.ImageBackground.attrs({
+export default styled.ImageBackground.attrs(props => ({
   source: background,
-  blurRadius: 2,
-})`
+  blurRadius: props.blurRadius || 2,
+}))`
   height: 100%;
   width: 100%;
   background-color: rgba(0, 0, 0, 0.45);
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -5,19 +5,46 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
 
 import { navigationRef } from './services/RootNavigation';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
+const dashboardOptions = {
+  tabBarLabel: 'Agendamentos',
+  tabBarIcon: () => <Icon name="event" size={20} color="#FFF" />,
+};
+
+const profileOptions = {
+  tabBarLabel: 'Meu perfil',
+  tabBarIcon: () => <Icon name="person" size={20} color="#FFF" />,
+};
+
+const tabBarOptions = {
+  keyboardHidesTabBar: true,
+  activeTintColor: '#FFF',
+  inactiveTintColor: 'rgba(255,255,255,0.2)',
+  style: {
+    backgroundColor: '#000',
+  },
+};
+
 function App() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Dashboard" component={Dashboard} />
+    <Tab.Navigator tabBarOptions={tabBarOptions}>
+      <Tab.Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={dashboardOptions}
+      />
+      <Tab.Screen name="Profile" component={Profile} options={profileOptions} />
     </Tab.Navigator>
   );
 }
