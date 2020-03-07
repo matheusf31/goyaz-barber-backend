7740cc7c7d2cb9e9a5693f0319e6d6774831a932
Matheus Filipe <mfcr031@gmail.com>
2020-03-07 09:11:31 -0300

    .

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 // baseURL via USB 4g:
 // baseURL via Genymotion: 'http://10.0.3.2:3333'
 const api = axios.create({
-  baseURL: 'http://192.168.43.223:3333',
+  baseURL: 'http://10.0.3.2:3333',
 });
 
 export default api;
