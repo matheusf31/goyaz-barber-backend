40057a16c5d172224fd37f993a9e0d8755ae7ec2
Matheus Filipe <mfcr031@gmail.com>
2020-05-06 11:46:53 -0300

    comentários removidos

--- mobile cliente/src/services/api.js
+++ mobile cliente/src/services/api.js
@@ -1,10 +1,5 @@
 import axios from 'axios';
 
-// baseURL via USB: 'http://192.168.0.14:3333'
-// baseURL via USB lord wi-fi: 'http://192.168.0.115:3333'
-// baseURL via USB 4g: 'http://192.168.43.223:3333'
-// baseURL via Genymotion: 'http://10.0.3.2:3333'
-// baseURL via Android Emulator: 'http://10.0.2.2:3333'
 const api = axios.create({
   baseURL: 'http://localhost:3333',
 });
