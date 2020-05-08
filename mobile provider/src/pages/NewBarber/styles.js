c21aa4f92024b8533420ca1755d04a5106b8c94b
Matheus Filipe <mfcr031@gmail.com>
2020-05-08 10:39:35 -0300

    página de adicionar barberio concluída

---
+++ mobile provider/src/pages/NewBarber/styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components/native';
+
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 20px;
+`;
+
+export const Title = styled.Text`
+  color: #fff;
+  font-size: 24px;
+  font-weight: bold;
+  margin: -104px 0 0;
+`;
+
+export const FormInput = styled(Input)`
+  margin-bottom: 10px;
+`;
+
+export const SubmitButton = styled(Button)`
+  margin-top: 20px;
+`;
+
+export const SignLink = styled.TouchableOpacity`
+  margin-top: 20px;
+
+  flex-direction: row;
+`;
