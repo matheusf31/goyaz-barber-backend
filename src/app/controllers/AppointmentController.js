c5a69c661a42d5b78b368adb422be4f3f572da6a
Matheus Filipe <mfcr031@gmail.com>
2020-05-21 09:47:38 -0300

    comentando envio de email

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -100,9 +100,9 @@ class AppointmentController {
 
     await appointment.save();
 
-    await Queue.add(CancellationMail.key, {
-      appointment,
-    });
+    // await Queue.add(CancellationMail.key, {
+    //   appointment,
+    // });
 
     return res.json(appointment);
   }
